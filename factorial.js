8fd2efe0ec461dd921f79fba42b3e93b3f94bc5f
Ashith Gannarapu <146576932+AshithGannarapu16@users.noreply.github.com>
2024-07-26 19:18:59 +0530

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,16 @@
+// function factorial(n){
+//     if(n === 0){
+//         return 1;
+//     }else{
+//         return n*factorial(n-1);
+//     }
+// }
+// console.log(factorial(5))
+
+
+// In Arrow Function:
+
+
+// const factorial=n=>(n===0?1:n*factorial(n-1));
+// console.log(factorial(5))
+
