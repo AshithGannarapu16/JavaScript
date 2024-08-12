6dbce8e18d90bab588886c163462675c9beca082
Ashith Gannarapu <146576932+AshithGannarapu16@users.noreply.github.com>
2024-08-12 23:34:39 +0530

    stingMeth

---
+++ stringmethods.js
@@ -0,0 +1,17 @@
+console.log("JavaScript testing".length);
+let str="Ashith";
+console.log(str.length);
+
+console.log("Ashith"[2]);
+
+console.log("JavaScript".charAt(2));
+
+let met="Sukhdev";
+console.log(met[2]);
+
+console.log("Ashith".toLowerCase());
+
+console.log("Ashith".toUpperCase());
+console.log("JavaScript".slice(2,-5));
+
+console.log("JavaScript".substring(2,6))
